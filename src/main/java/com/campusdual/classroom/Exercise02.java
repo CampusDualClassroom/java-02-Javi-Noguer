dd589fe0d02ae17302f5c108024d5b0428287314
JAVIER NOGUER FERNANDEZ <jnoguerfernandez@gmail.com>
2024-09-27 10:45:49 +0200

    Vale, ya lo hice

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -4,15 +4,15 @@ public class Exercise02 {
 
     public static void main(String[] args) {
         double r = 15;
-        double a, l;
+        double a= Math.PI*(r*r), l= 2*Math.PI*r;
 
         // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
 
-        System.out.println("Area: " + a = Math.PI*(r*r));
+        System.out.println("Area: " + a );
 
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
 
-        System.out.println("Longitud:  " + l = 2*Math.PI*r);
+        System.out.println("Longitud:  " + l );
 
     }
 }
