f63cae8fece0734db4f9ad90672d7136736a08a1
JAVIER NOGUER FERNANDEZ <jnoguerfernandez@gmail.com>
2024-09-27 10:29:37 +0200

    No sé porqué no me pasa el test. Las variables no están bien inicializadas?

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -5,14 +5,15 @@ public class Exercise02 {
     public static void main(String[] args) {
         double r = 15;
         double a, l;
+        double PI = 3.141;
 
         // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
 
-        System.out.println("Area: " + a);
+        System.out.println("Area: " + a = r*r* PI);
 
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
 
-        System.out.println("Longitud:  " + l);
+        System.out.println("Longitud:  " + l = 2*r* PI);
 
     }
 }
